e5dc279c0db94ba9190eb8e82c3dd5413045bd63
Andre Henrique Silva Lima <andrehenriquelima1986@gmail.com>
2024-02-19 10:38:00 -0300

    Criando estados no React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import Header from './componentes/Header'
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <AppContainer>
+      <Header />
+    </AppContainer>
   );
 }
 
-export default App;
+export default App
